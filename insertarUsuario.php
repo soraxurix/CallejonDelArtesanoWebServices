d4c1c239bc2615946b4760dc9a1972fe3601607b
soraxurix <71204059+soraxurix@users.noreply.github.com>
2021-06-06 02:25:21 -0500

    sad

--- insertarUsuario.php
+++ insertarUsuario.php
@@ -2,13 +2,28 @@
 // $nombre = "Olga";
 // $email = "olga@gmail.com";
 // $pass = "123";
+if (isset($_POST['nombre']) && isset($_POST['email']) && isset($_POST['pass'])) {
+  $nombre = $_POST['nombre'];
+  $email = $_POST['email'];
+  $pass = $_POST['pass'];
 
-$nombre = $_POST['nombre'];
-$email = $_POST['email'];
-$pass = $_POST['pass'];
+  include("connection.php");
 
-include("connection.php");
 
-$consulta = "INSERT INTO usuarios(Nombre, Email, pass) values('$nombre','$email','$pass')";
-$result = mysqli_query($con, $consulta);
+  $consulta = "SELECT COUNT(*) as contar  from usuarios where email = '$email'";
+  $result = mysqli_query($con,$consulta);
+  $array = mysqli_fetch_array($result);
+  echo $array['contar'];
+
+  if(!($array['contar'] == 1)){
+    $consulta = "INSERT INTO usuarios(Nombre, Email, pass) values('$nombre','$email','$pass')";
+    $result = mysqli_query($con, $consulta);
+    echo "Se insertó al usuario";
+  }else {
+    echo json_encode(null);
+  }
+}else{
+	echo json_encode(null);
+}
+
  ?>
