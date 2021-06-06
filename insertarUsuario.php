e346a28db078ce789bbacec388bbf90edf500b09
soraxurix <71204059+soraxurix@users.noreply.github.com>
2021-06-06 03:05:19 -0500

    3

--- insertarUsuario.php
+++ insertarUsuario.php
@@ -2,27 +2,24 @@
 // $nombre = "Olga";
 // $email = "uri";
 // $pass = "123";
-// if (isset($_POST['nombre']) && isset($_POST['email']) && isset($_POST['pass'])) {
-  $nombre = $_POST['nombre'];
-  $email = $_POST['email'];
-  $pass = $_POST['pass'];
 
-  include("connection.php");
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+include("connection.php");
 
 
-  $consulta = "SELECT COUNT(*) as contar  from usuarios where email = '$email'";
-  $result = mysqli_query($con,$consulta);
-  $array = mysqli_fetch_array($result);
+$consulta = "SELECT COUNT(*) as contar  from usuarios where email = '$email'";
+$result = mysqli_query($con,$consulta);
+$array = mysqli_fetch_array($result);
 
-  if(!($array['contar'] > 0)){
-    // $consulta = "INSERT INTO usuarios(Nombre, Email, pass) values('$nombre','$email','$pass')";
-    // $result = mysqli_query($con, $consulta);+
-    echo json_encode("Correcto");
-  }else {
-    echo json_encode(null);
-  }
-// }else{
-// 	echo json_encode(null);
-// }
+if(!($array['contar'] > 0)){
+  $consulta = "INSERT INTO usuarios(Nombre, Email, pass) values('$nombre','$email','$pass')";
+  $result = mysqli_query($con, $consulta);+
+  echo json_encode("Correcto");
+}else {
+  echo json_encode(null);
+}
 
  ?>
